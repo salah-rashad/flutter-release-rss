92bc93b177823a06ec5dffcd40545ba2d3a75497
Salah Rashad <35843293+salah-rashad@users.noreply.github.com>
2026-07-24 08:53:31 +0300

    Fix link formatting in release notification

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ async function fetchRelease(os) {
           title: `Flutter v${release["version"]} has been released`,
         },
         {
-          link: 'https://docs.flutter.dev/release/archive?tab=${os}',
+          link: `https://docs.flutter.dev/release/archive?tab=${os}`,
         },
         {
           description: `*Hash:* \`${release["hash"]}\` \n*Dart SDK Version:* \`${release["dart_sdk_version"]}\``,
