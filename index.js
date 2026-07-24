a7c1d96e8f24518d59502b6049429723b67d0a43
Salah Rashad <salah.r.ahmed@gmail.com>
2026-07-24 10:55:24 +0300

    Filter stable releases and remove duplicate versions

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ const query = ["windows", "macos", "linux"];
 async function fetchRelease(os) {
   const response = await fetch(`${baseUrl}/releases_${os}.json`);
   const data = await response.json();
-  const releases = data["releases"];
+  const seen = new Set();
+  const releases = data["releases"].filter((r) => {
+    if (r["channel"] !== "stable") return false;
+    if (seen.has(r["version"])) return false;
+    seen.add(r["version"]);
+    return true;
+  });
   const result = [];
   for (const release of releases) {
     result.push({
       item: [
         {
-          description: `*Flutter \`v${release["version"]}\` has been released*\n\n\`\`\`Hash:\t\t${release["hash"]}\nDart SDK:\t${release["dart_sdk_version"]}\`\`\`\n\n<https:\/\/docs.flutter.dev/release/release-notes/release-notes-${release["version"]}|Release Notes>`,
+          description: `*Flutter \`v${release["version"]}\` has been released*\n\n\`\`\`Hash:\t\t${release["hash"]}\nDart SDK:\t${release["dart_sdk_version"]}\`\`\`\n\n<https://docs.flutter.dev/release/release-notes/release-notes-${release["version"]}|Release Notes>`,
         },
         {
           pubDate: release["release_date"],
